b63f3aa0392c272bdc9f65d1044bbadd9b708ba4
Alexander <jcena1f.e@gmail.com>
2024-10-30 20:31:18 +0700

    fix

--- app/src/main/java/com/example/latihanuntukfragment/MainActivity.kt
+++ app/src/main/java/com/example/latihanuntukfragment/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.latihanuntukfragment
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +16,42 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val mFragmentManager = supportFragmentManager
+        val mfSatu = fSatu()
+
+        if (mFragmentManager.findFragmentByTag(fSatu::class.java.simpleName) == null) {
+            mFragmentManager
+                .beginTransaction()
+                .add(R.id.frameContainer, mfSatu, fSatu::class.java.simpleName)
+                .commit()
+        }
+
+        findViewById<Button>(R.id.btnHal1).setOnClickListener {
+            mFragmentManager
+                .beginTransaction()
+                .replace(R.id.frameContainer, mfSatu, fSatu::class.java.simpleName)
+                .addToBackStack(null)
+                .commit()
+        }
+
+        findViewById<Button>(R.id.btnHal2).setOnClickListener {
+            val mfDua = fDua()
+            mFragmentManager
+                .beginTransaction()
+                .replace(R.id.frameContainer, mfDua, fDua::class.java.simpleName)
+                .addToBackStack(null)
+                .commit()
+        }
+
+        findViewById<Button>(R.id.btnHal3).setOnClickListener {
+            val mfTiga = fTiga()
+            mFragmentManager
+                .beginTransaction()
+                .replace(R.id.frameContainer, mfTiga, fTiga::class.java.simpleName)
+                .addToBackStack(null)
+                .commit()
         }
     }
 }
